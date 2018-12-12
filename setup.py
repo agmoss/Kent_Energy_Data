4ecd0738655f462a7d7102d9e52622eca6b50a76
andrew <agordonmoss@gmail.com>
2018-12-12 14:32:54 -0700

    Seperating the project components

---
+++ setup.py
@@ -0,0 +1,56 @@
+
+"""
+A setuptools based setup module.
+
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Kent Energy Data',
+    version='0.1.0',
+    description='Web scraping and database storage',
+    long_description=long_description,
+    author='Andrew Moss',
+    author_email='agordonmoss@gmail.com',
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+        keywords='web scraping',
+        url='https://github.com/agmoss',
+        license=license,
+        packages=find_packages(exclude=('tests', 'docs'))
+)
